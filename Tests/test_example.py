ed33017135f1801ad26cfa7d60a479b1582a919d
Magnus Walter <magnus.walter@knowit.se>
2025-01-30 11:44:26 +0100

    Created fixure

--- Tests/test_example.py
+++ Tests/test_example.py
@@ -209,20 +209,13 @@ def test_go_to_kontakt_page(page:Page):
 
 @pytest.fixture(scope="function")
 def setup_pet_page(page) -> Page:
-    page = browser.new_page()
-    yield page
-    page.close()
-
-def test_add_and_get_pet(page: Page):
     # Define the path to the local AddPet HTML file
     add_pet_file_path = "file:///C:/Users/magwal/projects/PlayWrightForBeginners/petstore/AddPet.html"
-    # Define the path to the local GetPet HTML file
-    get_pet_file_path = "file:///C:/Users/magwal/projects/PlayWrightForBeginners/petstore/GetPet.html"
 
     # Generate a random Pet ID between 2000 and 3000
     pet_id = random.randint(2000, 3000)
     # Generate a random Pet Name
-    #pet_name = f"Pet{random.randint(1000, 9999)}"
+    # pet_name = f"Pet{random.randint(1000, 9999)}"
     pet_name = "Magnus"
     # Define the Photo URL
     photo_url = "https://images.dog.ceo/breeds/corgi-cardigan/n02113186_11035.jpg"
@@ -241,9 +234,22 @@ def test_add_and_get_pet(page: Page):
 
     # Wait for a success message or some indication that the pet was added
     page.wait_for_selector("text=Pet added successfully")
+    yield {"page": page, "pet_id": pet_id, "pet_name": pet_name, "photo_url": photo_url}
+    delete_button = page.get_by_role(role="button", name="Delete Pet")
+
+    delete_button.wait_for(timeout=3000, state="visible")
+    delete_button.click()
+    page.get_by_text("Pet deleted successfully.").wait_for(timeout=3000, state="visible")
+
+def test_add_and_get_pet(setup_pet_page):
+
+    pet_data = setup_pet_page
+    page = pet_data['page']
+    # Define the path to the local GetPet HTML file
+    get_pet_file_path = "file:///C:/Users/magwal/projects/PlayWrightForBeginners/petstore/GetPet.html"
 
     # Construct the URL with the PetID parameter for the GetPet page
-    get_pet_url = f"{get_pet_file_path}?petid={pet_id}"
+    get_pet_url = f"{get_pet_file_path}?petid={pet_data['pet_id']}"
     # Open the GetPet page
     page.goto(get_pet_url)
     # Wait for either the image or the message to be visible
@@ -252,12 +258,8 @@ def test_add_and_get_pet(page: Page):
     # Add assertions or further interactions as needed
     assert page.locator('img').is_visible() or page.locator('.message').is_visible()
     if page.locator('img').is_visible():
-        assert page.locator('img').get_attribute('src') == photo_url
+        assert page.locator('img').get_attribute('src') == pet_data['photo_url']
 
     assert page.locator('#pet-name').inner_text().endswith("Magnus")
 
-    delete_button = page.get_by_role(role="button", name="Delete Pet")
 
-    delete_button.wait_for(timeout=3000, state="visible")
-    delete_button.click()
-    page.get_by_text("Pet deleted successfully.").wait_for(timeout=3000, state="visible")
